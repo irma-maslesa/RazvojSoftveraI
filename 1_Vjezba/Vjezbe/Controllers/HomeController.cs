2b58251433b5b430abb14ddb8d7925c5955e6fc6
irma-maslesa <irma.maslesa@edu.fit.ba>
2020-11-08 12:32:32 +0100

    Vjezba 1 - kalkulator

---
+++ 1_Vjezba/Vjezbe/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vjezbe.Models;
+
+namespace Vjezbe.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Proba()
+        {
+            return "Oks je sve, no worry!";
+        }
+
+        public string Saberi(int a, int b)
+        {
+            return $"Jos je sve oks, a {a} + {b} je {a + b}";
+        }
+
+        public ViewResult HTMLSaberi(int a, int b)
+        {
+            return View("HTMLSaberiView", a+b);
+        }
+
+        public ViewResult SumaOpsega(int a, int b)
+        {
+            OpsegParametarModel p = new OpsegParametarModel { suma = 0, pocetak = a, kraj = b };
+
+            for (int i = a; i < b; i++)
+                p.suma += i;
+
+            return View("SumaOpsegaView", p);
+        }
+    }
+}
---
+++ 1_Vjezba/Vjezbe/Models/OpsegParametarModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vjezbe.Models
+{
+    public class OpsegParametarModel
+    {
+        public int pocetak;
+        public int kraj;
+        public int suma;
+    }
+}
